de26f1e1b29f6dbee641bf3ccbaf002b08e4a448
Allaniyazov Sultanbek <allaniyazov.sultan@mail.ru>
2022-05-24 12:25:22 +0600

    tel

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -3,25 +3,44 @@ const emailForm = document.getElementById('emailForm');
 emailForm.addEventListener('submit', sendMail);
 
 function sendMail(e) {
-    e.preventDefault()
+    e.preventDefault();
 
+    console.log
 
     const fullName = document.getElementById('fname').value;
 
     const pNumber = document.getElementById('pNumber').value;
 
 
-    Email.send({
-        SecureToken: "74c06a47-6f01-4dd8-a8e3-8d5c5d81847f",
-        To:   'allaniyazovsultan1@gmail.com',
-        From: 'sultanhahahamaratov@gmail.com',
-        Subject: "SnowLeopard Feedback contacts",
-        Body: `Name:              ${fullName}.<br>
-               Phone number:  ${pNumber}.`
-    }).then(console.log('The message has been sent!'));
-    
+    // Email.send({
+    //     SecureToken: "74c06a47-6f01-4dd8-a8e3-8d5c5d81847f",
+    //     To: 'allaniyazovsultan1@gmail.com',
+    //     From: 'sultanhahahamaratov@gmail.com',
+    //     Subject: "SnowLeopard Feedback contacts",
+    //     Body: `Name:              ${fullName}.<br>
+    //            Phone number:  ${pNumber}.`
+    // }).then(showAlert("Сообщение отправлено!"));
+    showAlert('Ваше сообщение отправлено успешно!')
+
     // sultanhahahamaratov@gmail.com
     // password 80AC4DD439A67D3C6CDC2F9DB585070706C3
 
     // abcdDCBA1234#
+    clearFields()
+}
+
+function showAlert(message) {
+    const div = document.createElement('div')
+    div.className = 'alert alert-success alert-message';
+    div.appendChild(document.createTextNode(message));
+
+    document.querySelector('.wrapper main').appendChild(div);
+
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector('.alert').remove(), 3000);
+}
+
+function clearFields() {
+    document.querySelector('#pNumber').value = '';
+    document.querySelector('#fname').value = '';
 }
